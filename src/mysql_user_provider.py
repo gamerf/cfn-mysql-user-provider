4b1663befd7f1d0e52bb7dd55bac8ac385b5d621
gamerf <50994049+gamerf@users.noreply.github.com>
2020-07-06 13:51:22 +0100

    Grant SELECT for RO users
    Update of mysql_user_provider.py to Grant SELECT for RO users only

--- src/mysql_user_provider.py
+++ src/mysql_user_provider.py
@@ -21,7 +21,6 @@ request_schema = {
         {"required": ["Database", "User", "PasswordSecretName"]}
     ],
     "properties": {
-        "Database": {"$ref": "#/definitions/connection"},
         "User": {
             "type": "string",
             "pattern": "^[_$A-Za-z][A-Za-z0-9_$]*(@[.A-Za-z0-9%_$\\-]+)?$",
@@ -42,16 +41,6 @@ request_schema = {
             "type": "string",
             "minLength": 1,
             "description": "the name of the password in the Secret Manager."
-        },
-        "WithDatabase": {
-            "type": "boolean",
-            "default": True,
-            "description": "create a database with the same name, or only a user"
-        },
-        "DeletionPolicy": {
-            "type": "string",
-            "default": "Retain",
-            "enum": ["Drop", "Retain"]
         }
     },
     "definitions": {
@@ -185,28 +174,13 @@ class MySQLUser(ResourceProvider):
         return self.get('Database', {}).get('User', None)
 
     @property
-    def with_database(self):
-        return self.get('WithDatabase', False)
-
-    @property
-    def deletion_policy(self):
-        return self.get('DeletionPolicy')
-
-    @property
     def connect_info(self):
         return {'host': self.host, 'port': self.port, 'database': self.dbname,
                 'user': self.dbowner, 'password': self.dbowner_password}
 
     @property
-    def allow_update(self):
-        return self.url == self.physical_resource_id
-
-    @property
     def url(self):
-        if self.with_database:
-            return 'mysql:%s:%s:%s:%s:%s' % (self.host, self.port, self.dbname, self.mysql_user, self.user)
-        else:
-            return 'mysql:%s:%s:%s::%s' % (self.host, self.port, self.dbname, self.user)
+        return 'mysql:%s:%s:%s::%s' % (self.host, self.port, self.dbname, self.user)
 
     def connect(self):
         log.info('connecting to database %s on port %d as user %s', self.host, self.port, self.dbowner)
@@ -220,125 +194,29 @@ class MySQLUser(ResourceProvider):
             self.connection.close()
             self.connection = None
 
-    def db_exists(self):
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute(
-                "SELECT SCHEMA_NAME FROM information_schema.schemata WHERE SCHEMA_NAME = %s", [self.mysql_user])
-            rows = cursor.fetchall()
-            return len(rows) > 0
-        finally:
-            cursor.close()
-
-    def user_exists(self):
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute(
-                "SELECT * FROM mysql.user WHERE user = %s AND host = %s", [self.mysql_user, self.mysql_user_host])
-            rows = cursor.fetchall()
-            return len(rows) > 0
-        finally:
-            cursor.close()
-
-    def drop_user(self):
-        cursor = self.connection.cursor()
-        try:
-            if self.deletion_policy == 'Drop':
-                log.info('drop user %s', self.user)
-                cursor.execute('DROP USER %s', [self.user])
-            else:
-                if self.is_5_7_or_higher():
-                    log.info('disable login of %s', self.user)
-                    cursor.execute("ALTER USER %s ACCOUNT LOCK", [self.user])
-                else:
-                    log.info('set random password for %s to disable login', self.user)
-                    cursor.execute("SET PASSWORD FOR %s = %s", [
-                        self.user,
-                        mysql_password(''.join(random.choices(string.ascii_uppercase + string.digits, k=16)))])
-        finally:
-            cursor.close()
-
-    def drop_database(self):
-        if self.deletion_policy == 'Drop':
-            log.info('drop database of %s', self.user)
-            cursor = self.connection.cursor()
-            try:
-                cursor.execute('DROP DATABASE %s' % self.mysql_user)
-            finally:
-                cursor.close()
-        else:
-            log.info('not dropping database %s', self.mysql_user)
-
-    def is_5_7_or_higher(self):
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute('select version()')
-            version = cursor.fetchone()[0].split('.')
-            return int(version[0]) >= 5 and int(version[1]) >= 7
-        except Exception as e:
-            self.fail('failed to determine database version, {}'.format(e))
-            raise e
-        finally:
-            cursor.close()
-
-    def update_password(self):
-        log.info('update password of user %s', self.user)
-        cursor = self.connection.cursor()
-        try:
-            if self.is_5_7_or_higher():
-                cursor.execute("ALTER USER %s IDENTIFIED BY %s ACCOUNT UNLOCK", [
-                    self.user, self.user_password])
-            else:
-                cursor.execute("SET PASSWORD FOR %s = %s", [
-                    self.user, mysql_password(self.user_password)])
-        finally:
-            cursor.close()
-
     def do_create_user(self):
         log.info('create user %s', self.user)
 
         cursor = self.connection.cursor()
         try:
-            cursor.execute('CREATE USER %s@%s IDENTIFIED BY %s', [
-                self.mysql_user, self.mysql_user_host, self.user_password])
+            cursor.execute("CREATE USER %s@'%' IDENTIFIED BY %s", [
+                self.mysql_user, self.user_password])
         finally:
             cursor.close()
-
-    def create_database(self):
-        log.info('create database %s', self.user)
+        
+    def grant_readonly(self):
+        log.info('grant readonly access on %s to %s', self.dbname, self.user)
         cursor = self.connection.cursor()
         try:
-            cursor.execute("CREATE DATABASE %s" % self.mysql_user)
+            cursor.execute("GRANT SELECT ON %s.* TO '%s'@'%'", [
+                self.dbname, self.mysql_user])
         finally:
             cursor.close()
-
-    def grant_ownership(self):
-        log.info('grant ownership on %s to %s', self.user, self.user)
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute("GRANT ALL ON %s.* TO '%s'@'%s' WITH GRANT OPTION" %
-                           (self.mysql_user, self.mysql_user, self.mysql_user_host))
-        finally:
-            cursor.close()
-
-    def drop(self):
-        if self.with_database and self.db_exists():
-            self.drop_database()
-        if self.user_exists():
-            self.drop_user()
 
     def create_user(self):
-        if self.user_exists():
-            self.update_password()
-        else:
+        if not self.user_exists():
             self.do_create_user()
-
-        if self.with_database:
-            if self.db_exists():
-                self.grant_ownership()
-            else:
-                self.create_database()
-                self.grant_ownership()
+            self.grant_readonly()
 
     def create(self):
         try:
@@ -348,31 +226,6 @@ class MySQLUser(ResourceProvider):
         except Exception as e:
             self.physical_resource_id = 'could-not-create'
             self.fail('Failed to create user, %s' % e)
-        finally:
-            self.close()
-
-    def update(self):
-        try:
-            self.connect()
-            if self.allow_update:
-                self.update_password()
-            else:
-                self.fail('Only the password of %s can be updated' % self.user)
-        except Exception as e:
-            self.fail('Failed to update the user, %s' % e)
-        finally:
-            self.close()
-
-    def delete(self):
-        if self.physical_resource_id == 'could-not-create':
-            self.success('user was never created')
-            return
-
-        try:
-            self.connect()
-            self.drop()
-        except Exception as e:
-            return self.fail(str(e))
         finally:
             self.close()
 
